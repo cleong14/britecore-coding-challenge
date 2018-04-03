73e1650334c301284804615fb7b393aae421f5f0
Chaz Leong <cleong14@gmail.com>
2018-04-03 01:40:49 -1000

    Working on getting cards to conditionally render cards based on search input results.

--- src/App.js
+++ src/App.js
@@ -6,29 +6,79 @@ import AddFieldModal from './Components/Modals/AddFieldModal';
 
 class App extends Component {
   state = {
-    open: false
+    activeTextButton: false,
+    activeDateButton: false,
   }
 
-  handleOpen = () => {
-    this.setState({open: true});
+  handleTextClick = (e, data) => {
+    console.log('ON CLICK DATA', data);
+    this.setState({activeTextButton: !this.state.activeTextButton});
   }
 
-  handleClose = () => {
-    this.setState({open: false});
+  handleDateClick = (e, data) => {
+    console.log('ON CLICK DATA', data);
+    this.setState({activeDateButton: !this.state.activeDateButton});
   }
 
-  // handleSave = () => {
-  //
-  // }
 
   render() {
+    let types = [
+      {
+        buttonClassName: 'textButton',
+        buttonOnClick: this.handleTextClick,
+        buttonActive: this.state.activeTextButton,
+        className: 'text',
+        label: 'Text',
+        definition: 'String of text',
+        defaultDisplay: 'Free-form text input'
+      },
+      {
+        buttonClassName: 'dateButton',
+        buttonOnClick: this.handleDateClick,
+        buttonActive: this.state.activeDateButton,
+        className: 'date',
+        label: 'Date',
+        definition: 'Standard ISO format date',
+        defaultDisplay: 'Datepicker, with configurable format'
+      },
+      // {
+      //   buttonClassName: 'vinButton',
+      //   className: 'vin',
+      //   label: 'VIN',
+      //   definition: 'Vehicle Identification Number',
+      //   defaultDisplay: 'Free-form text input'
+      // },
+      // {
+      //   buttonClassName: 'numberButton',
+      //   className: 'number',
+      //   label: 'Number',
+      //   definition: 'Integar',
+      //   defaultDisplay: 'Free-form integar input'
+      // },
+      // {
+      //   buttonClassName: 'currencyButton',
+      //   className: 'currency',
+      //   label: 'Currency',
+      //   definition: 'Form of currency',
+      //   defaultDisplay: 'Free-form currency input'
+      // },
+      // {
+      //   buttonClassName: 'selectButton',
+      //   className: 'select',
+      //   label: 'Select (Multiple preset options with a single choice)',
+      //   definition: 'Select single option from preset list',
+      //   defaultDisplay: 'Single choice from multiple preset options'
+      // }
+    ];
+
     console.log('APP STATE', this.state);
+
     return (
       <div className="App">
         <CommercialContainer>
           <h1 className="commercialHeader">Commercial Property</h1>
 
-          <AddFieldModal />
+          <AddFieldModal types={types} />
 
           {/* <h3>Preview Fields Below</h3>
           <FieldsContainer>
--- src/Components/Modals/AddFieldModal.jsx
+++ src/Components/Modals/AddFieldModal.jsx
@@ -1,30 +1,52 @@
 import React, { Component } from 'react';
-import { Grid, Button, Header, Modal } from 'semantic-ui-react';
+import { Grid, Button, Header, Modal, Input, Label } from 'semantic-ui-react';
+import Types from '../FieldTypes/Types';
 
 class AddFieldModal extends Component {
   state = {
-    open: false
+    modalOpen: false,
+    typeArray: this.props.types ? this.props.types : [],
+    renderArray: [],
+    currentSearch: ''
   }
 
   handleOpen = () => {
-    this.setState({open: true});
+    this.setState({modalOpen: true});
   }
 
   handleClose = () => {
-    this.setState({open: false});
+    this.setState({modalOpen: false});
   }
 
   // handleSave = () => {
   //
   // }
 
+  handleSearch = (e) => {
+    console.log(e.target.value);
+    this.setState({currentSearch: e.target.value});
+
+    // for(let i = 0; i < this.state.typeArray.length; i++){
+    //   console.log('EACH ITEM AFTER SEARCH', this.state.typeArray[i].className);
+    //   if(this.state.typeArray[i].className === this.state.currentSearch){
+    //     console.log('FOUND DA BUGGUH', this.state.typeArray[i]);
+    //     console.log('FOUND SEARCH', this.state.currentSearch);
+    //     this.state.renderArray.push(this.state.typeArray[i]);
+    //   }
+    // }
+  }
+
+  // TODO: TRY TO SET VARIABLE OUTSIDE RENDER TO CONDITIONALLY RENDER THAT WAY
+
   render() {
+    console.log('MODAL PROPS', this.props);
     console.log('MODAL STATE', this.state);
+
     return (
       <div className="AddFieldModal">
         <Modal
           trigger={<Button onClick={this.handleOpen}>Add Field</Button>}
-          open={this.state.open}
+          open={this.state.modalOpen}
           onClose={this.handleClose}
           size='fullscreen'
         >
@@ -35,7 +57,49 @@ class AddFieldModal extends Component {
                 <Grid.Column className='fieldTypes-column1'>
                   <h3>Field Types</h3>
 
-                  
+                  <div className='filterTypes'>
+                    <h5>Filter Types</h5><br/>
+                    <Input placeholder='Search types...' onKeyUp={this.handleSearch} />
+                  </div>
+
+                  {this.state.typeArray.map((type, i) => {
+                    console.log('EACH TYPE', type);
+                    if(this.state.currentSearch === type.className){
+                      console.log('GOT EM', type);
+                      console.log('GOT EM INDEX', i);
+                      this.state.renderArray.push(type);
+                      console.log('STATE AFTER PUSH', this.state);
+                      for(let k = 0; k < this.state.renderArray.length; k++){
+                        console.log('INSIDE RENDER ARR', this.state.renderArray[k]);
+                        return (
+                          <div key={i} className='types'>
+                            <Button className={this.state.renderArray[k].buttonClassName} toggle active={this.state.renderArray[k].buttonActive} onClick={this.state.renderArray[k].buttonOnClick}>
+                              <Types
+                                className={this.state.renderArray[k].className}
+                                label={this.state.renderArray[k].label}
+                                definition={this.state.renderArray[k].definition}
+                                defaultDisplay={this.state.renderArray[k].defaultDisplay}
+                              />
+                            </Button>
+                          </div>
+                        );
+                      }
+                    } else if(this.state.currentSearch !== type.className){
+                      console.log('RENDER ALL SINCE NO MATCH');
+                      return (
+                        <div key={i} className='types'>
+                          <Button className={type.buttonClassName} toggle active={type.buttonActive} onClick={type.buttonOnClick}>
+                            <Types
+                              className={type.className}
+                              label={type.label}
+                              definition={type.definition}
+                              defaultDisplay={type.defaultDisplay}
+                            />
+                          </Button>
+                        </div>
+                      );
+                    }
+                  })}
                 </Grid.Column>
               </Modal.Content>
             </Grid.Row>
