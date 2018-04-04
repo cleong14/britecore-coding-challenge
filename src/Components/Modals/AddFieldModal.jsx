824859da30751f63d5dd4e34013fdee7703ece68
Chaz Leong <cleong14@gmail.com>
2018-04-03 16:42:58 -1000

    Able to search and find known Types.

--- src/Components/Modals/AddFieldModal.jsx
+++ src/Components/Modals/AddFieldModal.jsx
@@ -7,7 +7,8 @@ class AddFieldModal extends Component {
     modalOpen: false,
     typeArray: this.props.types ? this.props.types : [],
     renderArray: [],
-    currentSearch: ''
+    currentSearch: '',
+    foundType: false
   }
 
   handleOpen = () => {
@@ -26,17 +27,29 @@ class AddFieldModal extends Component {
     console.log(e.target.value);
     this.setState({currentSearch: e.target.value});
 
-    // for(let i = 0; i < this.state.typeArray.length; i++){
-    //   console.log('EACH ITEM AFTER SEARCH', this.state.typeArray[i].className);
-    //   if(this.state.typeArray[i].className === this.state.currentSearch){
-    //     console.log('FOUND DA BUGGUH', this.state.typeArray[i]);
-    //     console.log('FOUND SEARCH', this.state.currentSearch);
-    //     this.state.renderArray.push(this.state.typeArray[i]);
-    //   }
-    // }
-  }
+    this.state.typeArray.map((type) => {
+      console.log('TYPE IN SEARCH', type);
+      if(e.target.value === type.className){
+        console.log('MATCH');
+        this.setState({foundType: true});
+        this.state.renderArray.push(type);
+        console.log('STATE AT END OF FOUND', this.state);
+      }
+      console.log('STATE OUTSIDE OF FOUND CONDITIONAL', this.state);
+      if(e.target.value !== type.className){
+        if(this.state.foundType){
+          console.log('NOW NOT SAME');
+          console.log(this.state.foundType);
+          this.state.renderArray.splice(0, 1);
+          this.setState({foundType: false});
+        }
+      }
+    });
 
-  // TODO: TRY TO SET VARIABLE OUTSIDE RENDER TO CONDITIONALLY RENDER THAT WAY
+    this.state.renderArray.map((renderType) => {
+      console.log('TYPE IN RENDER ARR', renderType);
+    });
+  }
 
   render() {
     console.log('MODAL PROPS', this.props);
@@ -62,30 +75,31 @@ class AddFieldModal extends Component {
                     <Input placeholder='Search types...' onKeyUp={this.handleSearch} />
                   </div>
 
-                  {this.state.typeArray.map((type, i) => {
-                    console.log('EACH TYPE', type);
-                    if(this.state.currentSearch === type.className){
-                      console.log('GOT EM', type);
-                      console.log('GOT EM INDEX', i);
-                      this.state.renderArray.push(type);
-                      console.log('STATE AFTER PUSH', this.state);
-                      for(let k = 0; k < this.state.renderArray.length; k++){
-                        console.log('INSIDE RENDER ARR', this.state.renderArray[k]);
-                        return (
-                          <div key={i} className='types'>
-                            <Button className={this.state.renderArray[k].buttonClassName} toggle active={this.state.renderArray[k].buttonActive} onClick={this.state.renderArray[k].buttonOnClick}>
-                              <Types
-                                className={this.state.renderArray[k].className}
-                                label={this.state.renderArray[k].label}
-                                definition={this.state.renderArray[k].definition}
-                                defaultDisplay={this.state.renderArray[k].defaultDisplay}
-                              />
-                            </Button>
-                          </div>
-                        );
-                      }
-                    } else if(this.state.currentSearch !== type.className){
-                      console.log('RENDER ALL SINCE NO MATCH');
+                  {this.state.renderArray.map((type, i) => {
+                    console.log('TYPE IN RENDER ARRAY', type);
+                    // console.log('EACH TYPE', type);
+                    // if(this.state.currentSearch === type.className){
+                    //   console.log('GOT EM', type);
+                    //   console.log('GOT EM INDEX', i);
+                    //   this.state.renderArray.push(type);
+                    //   console.log('STATE AFTER PUSH', this.state);
+                    //   for(let k = 0; k < this.state.renderArray.length; k++){
+                    //     console.log('INSIDE RENDER ARR', this.state.renderArray[k]);
+                    //     return (
+                    //       <div key={i} className='types'>
+                    //         <Button className={this.state.renderArray[k].buttonClassName} toggle active={this.state.renderArray[k].buttonActive} onClick={this.state.renderArray[k].buttonOnClick}>
+                    //           <Types
+                    //             className={this.state.renderArray[k].className}
+                    //             label={this.state.renderArray[k].label}
+                    //             definition={this.state.renderArray[k].definition}
+                    //             defaultDisplay={this.state.renderArray[k].defaultDisplay}
+                    //           />
+                    //         </Button>
+                    //       </div>
+                    //     );
+                    //   }
+                    // } else if(this.state.currentSearch !== type.className){
+                    //   console.log('RENDER ALL SINCE NO MATCH');
                       return (
                         <div key={i} className='types'>
                           <Button className={type.buttonClassName} toggle active={type.buttonActive} onClick={type.buttonOnClick}>
@@ -98,7 +112,7 @@ class AddFieldModal extends Component {
                           </Button>
                         </div>
                       );
-                    }
+                    // }
                   })}
                 </Grid.Column>
               </Modal.Content>
